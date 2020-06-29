444cc946280ca1ad657daaa7a91d055b9955fd22
JAlex00 <62698161+JAlex00@users.noreply.github.com>
2020-06-29 10:28:47 +0200

    Add files via upload

---
+++ Movimento.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Movimento {
+
+	private String op;
+	private String idConto;
+	private String mov;
+	
+	public String getOp() {
+		return op;
+	}
+	public void setOp(String op) {
+		this.op = op;
+	}
+	public String getIdConto() {
+		return idConto;
+	}
+	public void setIdConto(String idConto) {
+		this.idConto = idConto;
+	}
+	public String getMov() {
+		return mov;
+	}
+	public void setMov(String mov) {
+		this.mov = mov;
+	}
+	
+}
